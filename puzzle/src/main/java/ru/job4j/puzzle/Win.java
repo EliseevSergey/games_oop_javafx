93a1e8f9f9cad85a72077b4a17a4fa2168d9b127
sergeyeliseev <s.eliseev1@gmail.com>
2020-11-30 20:58:00 +0300

    2. Сокобан. Тестовое задание. [#396509]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,30 +1,41 @@
 package ru.job4j.puzzle;
 
 public class Win {
-    public static boolean check(int[][] board) {
-        boolean rsl = false;
+    public static boolean monoVertical(int[][] board) {
+        boolean rsl = true;
         for (int diagonal = 0; diagonal < board.length; diagonal++) {
-            if (board[diagonal][diagonal] == 1 && board[diagonal + 1][diagonal] == 1) { // проверка вертикальной правильности
+            if (board[diagonal][diagonal] == 1) {
                 for (int row = 0; row < board.length; row++) {
-                    if (board[row][diagonal] == 1) {
-                        rsl = true;
-                    } else {
+                    if (board[row][diagonal] != 1) {
                         rsl = false;
                         break;
                     }
                 }
             }
-            if ((board[diagonal][diagonal] == 1 && board[diagonal][diagonal + 1] == 1)) { // проверка горизонтальной правильности
-            for (int cell = 0; cell < board.length; cell++) {
-                if (board[diagonal][cell] == 1) {
-                    rsl = true;
-                } else {
-                    rsl = false;
-                    break;
+        }
+        return rsl;
+    }
+
+    public static boolean monoHorizontal(int[][] board) {
+        boolean rsl = true;
+        for (int diagonal = 0; diagonal < board.length; diagonal++) {
+            if (board[diagonal][diagonal] == 1) {
+                for (int cell = 0; cell < board.length; cell++) {
+                    if (board[diagonal][cell] != 1) {
+                        rsl = false;
+                        break;
+                    }
                 }
             }
         }
+        return rsl;
     }
+
+    public static boolean check(int[][] board) {
+        boolean rsl = false;
+        if ((monoHorizontal(board)) || monoVertical(board)) {
+            rsl = true;
+        }
         return rsl;
     }
 }
